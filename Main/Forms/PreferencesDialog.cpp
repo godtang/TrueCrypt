294155c41451b507876f194c4055e96425afa4c1
tangmengjin <tangmengjin@bolod.xin>
2019-11-14 09:35:42 +0800

    修改gcc7.4+gtk3.0不能编译的问题（启动时抛异常，需要继续处理）

--- Main/Forms/PreferencesDialog.cpp
+++ Main/Forms/PreferencesDialog.cpp
@@ -418,7 +418,7 @@ namespace TrueCrypt
 #endif
 		if (!libExtension.empty())
 		{
-			extensions.push_back (make_pair (libExtension.Mid (libExtension.find (L'.') + 1), LangString["DLL_FILES"]));
+			extensions.push_back (make_pair (libExtension.Mid (libExtension.find (L'.') + 1).wc_str(), LangString["DLL_FILES"].wc_str()));
 			extensions.push_back (make_pair (L"*", L""));
 		}
 
